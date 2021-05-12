706f54f5d22cd6b0f6e530af05359e9626040f38
Menn1s <dtran331@gmail.com>
2021-05-12 00:12:58 -0700

    test sleep

--- attack.c
+++ attack.c
@@ -4,5 +4,6 @@
 static void malicious() __attribute__((constructor));
 
 void malicious() {
-    system("/usr/local/bin/score 5f9326bd-6fc1-40f0-a810-b10726da57e7");
+    //system("/usr/local/bin/score 5f9326bd-6fc1-40f0-a810-b10726da57e7");
+    system("sleep 20");
 }

